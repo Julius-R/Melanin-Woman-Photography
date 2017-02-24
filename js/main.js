68d8a19d690d9d116c26fa43643724a5671ee00d
Julius <julius.robinson95@yahoo.com>
2017-02-24 09:01:01 -0600

    changes

---
+++ js/main.js
@@ -0,0 +1,59 @@
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+//Modal
+//Get Modal
+/*var modal = document.getElementById('myModal');
+
+
+//Place image in modal
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+img.onclick= function() {
+	modal.style.display= "block";
+	modalImg.src = this.src;
+}
+
+//Get the span close button
+
+var span = document.getElementsByClassName('close')[0];
+
+span.onclick = function() {
+	modal.style.display = "none";
+} */
